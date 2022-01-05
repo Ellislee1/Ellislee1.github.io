482860a4f90c902b022e10c094f3cfa46454c2d2
Ellis Thompson <>
2022-01-05 16:53:53 +0000

    new landing page

--- src/components/App.js
+++ src/components/App.js
@@ -9,52 +9,33 @@ function App() {
       <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"></link>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Serif"></link>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"></link>
       </head>
     <div class="page-content">
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark nav-colour">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">Ellis Thompson</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
+            <Link class="navbar-brand" to="/">Ellis Thompson</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
+            <div class="collapse navbar-collapse" id="navbarText">
+              <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                  <Link class="nav-link active" to="/">Home</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link active" to="/cv">Digital CV</Link>
+                  <Link to="/" class="nav-link" href="#">Home</Link>
                 </li>
               </ul>
+              <div class="navbar-right">
+                <a href="./assets/Ellis_Thompson_CV.pdf" download="Ellis Thompson CV.pdf" class="btn btn-outline-light">Download CV</a>
+              </div>
             </div>
-          </div>
         </nav>
       </header>
 
       <main>
         <div class="content">
-            <div class="content-wrap">
-              <Outlet />
-            </div>
+          <Outlet />
         </div>
       </main>
-
-      <footer class="w-100 py-4 flex-shrink-0">
-        <div class="container py-4">
-            <div class="row gy-4 gx-5">
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="h1 text-white">Ellis Thompson</h5>
-                    {/* <p class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p> */}
-                    <p class="small text-muted mb-0">&copy; Ellis Thompson. All rights reserved.</p>
-                </div>
-                <div class="col-lg-3 col-md-6 text-center text-white">
-                  <Link to="/">Home</Link> | <Link to="/cv">Digital CV</Link>
-                </div>
-            </div>
-        </div>
-    </footer>
     </div>
 
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Main() {
     return (
       <div className="Main">
-          <h1>Ellis Thompson</h1>
+        <div class="padding-3">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center align-items-center">
+                <div class="typewriter constrained">
+                  <h1 id="title">Ellis Thompson</h1>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center align-items-center">
+              < h3 class="subtitle">Ph.D. Computer Science Student</h3>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="container ">
+            <div class="row d-flex justify-content-center align-items-center">
+              <div class="col-8">
+                <p>
+                  I'm Ellis, a Ph.D. candidate at George Washington University (Washington, DC), studying Computer Science. I have Masters degree in Computer Science from Swansea University (2021) and currently have a research focus in using Reinforcement Learning for Air Traffic Management (ATM).
+                </p>
+                <p>
+                  My classes focus on related topics with a drive in Machine Learning and AI based modules but I have taken an interest in Programming (C++/C#) and Data Visualisation/Manipulation modules. 
+                </p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        
       </div>
     );
   }
