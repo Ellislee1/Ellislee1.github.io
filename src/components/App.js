f2e311a68b023b842edc666833140631cf7feb61
Ellis Thompson <>
2022-01-05 17:11:12 +0000

    better flex

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,20 @@ function App() {
   return (
     <div class="App" className="App">
       <head>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"></link>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"></link>
       </head>
     <div class="page-content">
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
+        <nav class="navbar navbar-expand navbar-dark bg-custom">
             <Link class="navbar-brand" to="/">Ellis Thompson</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> */}
             <div class="collapse navbar-collapse" id="navbarText">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,27 +6,38 @@ function Main() {
         <div class="padding-3">
           <div class="container">
             <div class="row">
-              <div class="col-12 d-flex justify-content-center align-items-center">
+              <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
                 <div class="typewriter constrained">
                   <h1 id="title">Ellis Thompson</h1>
                 </div>
               </div>
+              <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
+                <div class="typewriter constrained">
+                  <h1 id="title2">Ellis Thompson</h1>
+                </div>
+              </div>
             </div>
             <div class="row">
-              <div class="col-12 d-flex justify-content-center align-items-center">
-              < h3 class="subtitle">Ph.D. Computer Science Student</h3>
+            <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
+                < h3 class="subtitle">Ph.D. Computer Science Student</h3>
+              </div>
+              <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
+                < h3 class="subtitle2">Ph.D. Computer Science Student</h3>
               </div>
             </div>
           </div>
           <br />
           <div class="container ">
             <div class="row d-flex justify-content-center align-items-center">
-              <div class="col-8">
+              <div class="col-md-8 col-12">
                 <p>
                   I'm Ellis, a Ph.D. candidate at George Washington University (Washington, DC), studying Computer Science. I have Masters degree in Computer Science from Swansea University (2021) and currently have a research focus in using Reinforcement Learning for Air Traffic Management (ATM).
                 </p>
                 <p>
                   My classes focus on related topics with a drive in Machine Learning and AI based modules but I have taken an interest in Programming (C++/C#) and Data Visualisation/Manipulation modules. 
+                </p>
+                <p>
+                  My current advisor is <a href="https://www.seas.gwu.edu/peng-wei" target="blank_">Dr. Peng Wei</a> 
                 </p>
               </div>
             </div>
