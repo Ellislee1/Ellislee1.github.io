42b6c8519575b4945045ab749240cff6e0645c81
Ellis Thompson <>
2022-01-06 14:05:22 +0000

    fixed splelling

--- src/components/DigitalCV.js
+++ src/components/DigitalCV.js
@@ -1,174 +1,175 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+ 
 function Main() {
-    return (
-      <div className="DigitalCV px-5">
-          <h1 id="title">Digital CV</h1>
-          <a href="https://drive.google.com/uc?export=download&amp;id=1Pf1rzJ6c8-tt9JLngzQ8i88FJEGLebB5" download="Ellis Thompson CV.pdf">Download</a>
-          <hr/>
-          <h3>Research Interests</h3>
-          <div class="text-center">
-            <p class="text-center">Reinforcement Learning | Machine Learning | Multi-Agent Reinforcement Learning | Computer Vision</p>
-            <p class="text-center">Air Trafic Managament (ATM) | UAS Traffic Managament (UTM) | Autonomous Transport Systems</p>
-          </div>
-
-          <hr/>
-          <h3>Education</h3>
-          <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h4 class="item-header">George Washington University</h4>
-                </div>
-                <div class="col justify-right">
-                    <p class="font-italic">Aug 2021 - Present</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col justify-left">
-                    <p class="font-italic">Ph.D. Computer Science</p>
-                </div>
-                <div class="col">
-                    <p class="indent-10">Advisor(s): <a href="https://www.seas.gwu.edu/peng-wei" target="_blank">Dr. Peng Wei</a></p>
-                </div>
-            </div>
-          </div>
-          <br/>
-          <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h4 class="item-header">Swansea University</h4>
-                </div>
-                <div class="col justify-right">
-                    <p class="font-italic">Sep 2018 - June 2021</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col justify-left">
-                    <p class="font-italic">MSc. Computer Science</p>
-                </div>
-                <div class="col">
-                    <p class="indent-10">Advisor(s): <a href="https://www.swansea.ac.uk/staff/science/compsci/muller-b/" target="_blank">Dr. Bertie Muller</a></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p class="indent-10">Dissertation: Reinforcement Learning for Air Traffic Management</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p class="indent-10">Award Achieved: 1st Class (Hons)</p>
-                </div>
-            </div>
-          </div>
-          <br/>
-          <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h4 class="item-header">University of Chester</h4>
-                </div>
-                <div class="col justify-right">
-                    <p class="font-italic">Sep 2017 - June 2018</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p class="indent-10">CertHE in Computer Science</p>
-                </div>
-            </div>
-          </div>
-
-          <hr/>
-          <h3>Research Experience</h3>
-          <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h4 class="item-header">George Washington University</h4>
-                </div>
-                <div class="col justify-right">
-                    <p class="font-italic">Washington, DC</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col justify-left">
-                    <p class="font-italic">Ph.D. Student &amp; Graduate Research Assistant</p>
-                </div>
-                <div class="col">
-                    <p class="font-italic">Aug 2021 - Present</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-8">
-                    <ul>
-                        <li class="j-text">Designing a risk assessment based system to be part of an in-time learning-based aviation safety management system (ILASMS) for scalable advanced air mobility (AAM) on a NASA funded project.</li>
-                        <li class="j-text">Performing research on autonomous control systems for traffic management in the terminal airspace.</li>
-                    </ul>
-                </div>
-            </div>
-          </div>
-          <br />
-          <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h4 class="item-header">Swansea University</h4>
-                </div>
-                <div class="col justify-right">
-                    <p class="font-italic">Swansea, UK</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col justify-left">
-                    <p class="font-italic">MSc Student</p>
-                </div>
-                <div class="col">
-                    <p class="font-italic">Sep 2019 - June 2021</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-8">
-                    <ul>
-                        <li class="j-text">Adapted an existing conflict avoidance system to utilise altitude changes to avoid conflict states between en-route aircraft.</li>
-                        <li class="j-text">Performed research into possible solutions for utilising airspace sectors for autonomous air traffic management, utilising reinforcement learning.</li>
-                    </ul>
-                </div>
-            </div>
-          </div>
-
-          <hr/>
-          <h3>Selected Courseworks</h3>
-          <div class="container">
-              <div class="row d-flex justify-content-center">
-                  <div class="col-md-8 d-none d-md-flex">
-                    <ul class = "list-unstyled double-col">
-                        <li>Machine Learning (Swansea)</li>
-                        <li>Big Data and Data Mining (Swansea)</li>
-                        <li>Data Visualisation (Swansea)</li>
-                        <li>Mathematics for Data Science (Swansea)</li>
-                    </ul>
-                  </div>
-                  <div class="col-12 d-flex d-md-none">
-                    <ul class = "list-unstyled">
-                        <li>Machine Learning (Swansea)</li>
-                        <li>Big Data and Data Mining (Swansea)</li>
-                        <li>Data Visualisation (Swansea)</li>
-                        <li>Mathematics for Data Science (Swansea)</li>
-                    </ul>
-                  </div>
-              </div>
-          </div>
-          
-
-          <hr/>
-          <h3>Skills</h3>
-          <div class="container">
-                <div class="row d-flex justify-content-center">
-                  <div class="col-12 col-md-8">
-                    <p class="t-center">Python(TensorFlow, PyTorch, SciKitLearn) | C# | Java | C++ | HTML | JavaScript(Node.Js) | Tableu</p>
-                  </div>
-              </div>
-            </div>
-      </div>
-    );
-  }
-  
+   return (
+     <div className="DigitalCV px-5">
+         <h1 id="title">Digital CV</h1>
+         <a href="https://drive.google.com/uc?export=download&amp;id=1Pf1rzJ6c8-tt9JLngzQ8i88FJEGLebB5" download="Ellis Thompson CV.pdf">Download</a>
+         <hr/>
+         <h3>Research Interests</h3>
+         <div class="text-center">
+           <p class="text-center">Reinforcement Learning | Machine Learning | Multi-Agent Reinforcement Learning | Computer Vision</p>
+           <p class="text-center">Air Traffic Management (ATM) | UAS Traffic Management (UTM) | Autonomous Transport Systems</p>
+         </div>
+ 
+         <hr/>
+         <h3>Education</h3>
+         <div class="container">
+           <div class="row">
+               <div class="col">
+                   <h4 class="item-header">George Washington University</h4>
+               </div>
+               <div class="col justify-right">
+                   <p class="font-italic">Aug 2021 - Present</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col justify-left">
+                   <p class="font-italic">Ph.D. Computer Science</p>
+               </div>
+               <div class="col">
+                   <p class="indent-10">Advisor(s): <a href="https://www.seas.gwu.edu/peng-wei" target="_blank">Dr. Peng Wei</a></p>
+               </div>
+           </div>
+         </div>
+         <br/>
+         <div class="container">
+           <div class="row">
+               <div class="col">
+                   <h4 class="item-header">Swansea University</h4>
+               </div>
+               <div class="col justify-right">
+                   <p class="font-italic">Sep 2018 - June 2021</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col justify-left">
+                   <p class="font-italic">MSc. Computer Science</p>
+               </div>
+               <div class="col">
+                   <p class="indent-10">Advisor(s): <a href="https://www.swansea.ac.uk/staff/science/compsci/muller-b/" target="_blank">Dr. Bertie Muller</a></p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col">
+                   <p class="indent-10">Dissertation: Reinforcement Learning for Air Traffic Management</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col">
+                   <p class="indent-10">Award Achieved: 1st Class (Hons)</p>
+               </div>
+           </div>
+         </div>
+         <br/>
+         <div class="container">
+           <div class="row">
+               <div class="col">
+                   <h4 class="item-header">University of Chester</h4>
+               </div>
+               <div class="col justify-right">
+                   <p class="font-italic">Sep 2017 - June 2018</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col">
+                   <p class="indent-10">CertHE in Computer Science</p>
+               </div>
+           </div>
+         </div>
+ 
+         <hr/>
+         <h3>Research Experience</h3>
+         <div class="container">
+           <div class="row">
+               <div class="col">
+                   <h4 class="item-header">George Washington University</h4>
+               </div>
+               <div class="col justify-right">
+                   <p class="font-italic">Washington, DC</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col justify-left">
+                   <p class="font-italic">Ph.D. Student &amp; Graduate Research Assistant</p>
+               </div>
+               <div class="col">
+                   <p class="font-italic">Aug 2021 - Present</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-12 col-md-8">
+                   <ul>
+                       <li class="j-text">Designing a risk assessment based system to be part of an in-time learning-based aviation safety management system (ILASMS) for scalable advanced air mobility (AAM) on a NASA funded project.</li>
+                       <li class="j-text">Performing research on autonomous control systems for traffic management in the terminal airspace.</li>
+                   </ul>
+               </div>
+           </div>
+         </div>
+         <br />
+         <div class="container">
+           <div class="row">
+               <div class="col">
+                   <h4 class="item-header">Swansea University</h4>
+               </div>
+               <div class="col justify-right">
+                   <p class="font-italic">Swansea, UK</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col justify-left">
+                   <p class="font-italic">MSc Student</p>
+               </div>
+               <div class="col">
+                   <p class="font-italic">Sep 2019 - June 2021</p>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-12 col-md-8">
+                   <ul>
+                       <li class="j-text">Adapted an existing conflict avoidance system to utilise altitude changes to avoid conflict states between en-route aircraft.</li>
+                       <li class="j-text">Performed research into possible solutions for utilising airspace sectors for autonomous air traffic management, utilising reinforcement learning.</li>
+                   </ul>
+               </div>
+           </div>
+         </div>
+ 
+         <hr/>
+         <h3>Selected Courseworks</h3>
+         <div class="container">
+             <div class="row d-flex justify-content-center">
+                 <div class="col-md-8 d-none d-md-flex">
+                   <ul class = "list-unstyled double-col">
+                       <li>Machine Learning (Swansea)</li>
+                       <li>Big Data and Data Mining (Swansea)</li>
+                       <li>Data Visualisation (Swansea)</li>
+                       <li>Mathematics for Data Science (Swansea)</li>
+                   </ul>
+                 </div>
+                 <div class="col-12 d-flex d-md-none">
+                   <ul class = "list-unstyled">
+                       <li>Machine Learning (Swansea)</li>
+                       <li>Big Data and Data Mining (Swansea)</li>
+                       <li>Data Visualisation (Swansea)</li>
+                       <li>Mathematics for Data Science (Swansea)</li>
+                   </ul>
+                 </div>
+             </div>
+         </div>
+        
+ 
+         <hr/>
+         <h3>Skills</h3>
+         <div class="container">
+               <div class="row d-flex justify-content-center">
+                 <div class="col-12 col-md-8">
+                   <p class="t-center">Python(TensorFlow, PyTorch, SciKitLearn) | C# | Java | C++ | HTML | JavaScript(Node.Js) | Tableau</p>
+                 </div>
+             </div>
+           </div>
+     </div>
+   );
+ }
   export default Main;
+ 
+
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,56 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import personal from '../assets/personal.jpg'
-
+ 
 function Main() {
-    return (
-      <div className="Main">
-        <div class="padding-3">
-          <div class="container">
-            <div class="row">
-              <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
-                <div class="typewriter constrained">
-                  <h1 id="title">Ellis Thompson</h1>
-                </div>
-              </div>
-              <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
-                <div class="typewriter constrained">
-                  <h1 id="title2">Ellis Thompson</h1>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-            <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
-                < h3 class="subtitle">Ph.D. Computer Science Student</h3>
-              </div>
-              <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
-                < h3 class="subtitle2">Ph.D. Computer Science Student</h3>
-              </div>
-            </div>
-            <img src={personal} class="mx-auto d-block profile"></img>
-          </div>
-          {/* <img src="https://drive.google.com/uc?id=1n15Fxm8Mh9qqOYDIpLsD5M1uOTglspwY" class="img-fluid" alt="Me"></img> */}
-          
-          <br />
-          <div class="container ">
-            <div class="row d-flex justify-content-center align-items-center">
-              <div class="col-md-8 col-12">
-                <p>
-                  I'm Ellis, a Ph.D. candidate at George Washington University (Washington, DC), studying Computer Science. I have Masters degree in Computer Science from Swansea University (2021) and currently have a research focus in using Reinforcement Learning for Air Traffic Management (ATM).
-                </p>
-                <p>
-                  My current advisor is <a href="https://www.seas.gwu.edu/peng-wei" target="blank_">Dr. Peng Wei</a> and my current research projects are investigating ways we can expand and improve how we control air traffic, both commercial and autonomous vehicles, namely through Reinforcement Learning.
-                </p>
-                <p>
-                  My classes focus on related topics with a drive in Machine Learning and AI based modules but I have taken an interest in Programming (C++/C#) and Data Visualisation/Manipulation modules. 
-                </p>
-              </div>
-            </div>
-          </div>
-          
-        </div>
+   return (
+     <div className="Main">
+       <div class="padding-3">
+         <div class="container">
+           <div class="row">
+             <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
+               <div class="typewriter constrained">
+                 <h1 id="title">Ellis Thompson</h1>
+               </div>
+             </div>
+             <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
+               <div class="typewriter constrained">
+                 <h1 id="title2">Ellis Thompson</h1>
+               </div>
+             </div>
+           </div>
+           <div class="row">
+           <div class="d-none col-12 d-md-flex justify-content-center align-items-center">
+               < h3 class="subtitle">Ph.D. Computer Science Student</h3>
+             </div>
+             <div class="d-flex col-12 d-md-none justify-content-center align-items-center">
+               < h3 class="subtitle2">Ph.D. Computer Science Student</h3>
+             </div>
+           </div>
+           <img src={personal} class="mx-auto d-block profile"></img>
+         </div>
+         {/* <img src="https://drive.google.com/uc?id=1n15Fxm8Mh9qqOYDIpLsD5M1uOTglspwY" class="img-fluid" alt="Me"></img> */}
         
-      </div>
-    );
-  }
-  
+         <br />
+         <div class="container ">
+           <div class="row d-flex justify-content-center align-items-center">
+             <div class="col-md-8 col-12">
+               <p>
+                 I'm Ellis, a Ph.D. candidate at George Washington University (Washington, DC), studying Computer Science. I have a Masters degree in Computer Science from Swansea University (2021) and currently have a research focus in using Reinforcement Learning for Air Traffic Management (ATM).
+               </p>
+               <p>
+                 My current advisor is <a href="https://www.seas.gwu.edu/peng-wei" target="blank_">Dr. Peng Wei</a> and my current research projects are investigating ways we can expand and improve how we control air traffic, both commercial and autonomous vehicles, namely through Reinforcement Learning.
+               </p>
+               <p>
+                 My classes focus on related topics with a drive in Machine Learning and AI based modules but I have taken an interest in Programming (C++/C#) and Data Visualisation/Manipulation modules.
+               </p>
+             </div>
+           </div>
+         </div>
+        
+       </div>
+      
+     </div>
+   );
+ }
   export default Main;
+
